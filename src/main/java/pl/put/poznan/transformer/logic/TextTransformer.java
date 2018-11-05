3ce8986bed055f52880c7bd6fe21e10c5b58f5f5
mdziardziel <mdziardziel@gmail.com>
2018-11-05 19:50:09 +0100

    add removeRepetitions method

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,5 +1,8 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
@@ -14,5 +17,26 @@ public class TextTransformer {
     public String transform(String text){
         // of course normally it would to something based on transforms
         return text.toUpperCase();
+    }
+
+    private String removeRepetitions(String sentence){
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
+        int number_of_words = words.size();
+        int i = 0;
+        while(i + 1 < number_of_words){
+            char[] word1 = words.get(i).replaceAll("[^a-zA-Z ]", "").toLowerCase().toCharArray();
+            char[] word2 = words.get(i+1).replaceAll("[^a-zA-Z ]", "").toLowerCase().toCharArray();
+            if(Arrays.equals(word1, word2)){
+                if(words.get(i).length() < words.get(i+1).length()){
+                    words.remove(i);
+                } else {
+                    words.remove(i+1);
+                }
+                number_of_words--;
+            }else {
+                i++;
+            }
+        }
+        return String.join(" ", words);
     }
 }
