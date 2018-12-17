de2505a2296bb678c4985d000669ce0d86c34ada
MichalDolata <dev@dolata.me>
2018-12-17 20:25:57 +0100

    Add tests to upper and lower transformer

---
+++ src/test/java/pl/put/poznan/transformer/logic/Decorators/LowerTransformerTest.java
@@ -0,0 +1,29 @@
+package pl.put.poznan.transformer.logic.Decorators;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.put.poznan.transformer.logic.BaseTransformer;
+import pl.put.poznan.transformer.logic.Decorators.LowerTransformer;
+
+public class LowerTransformerTest {
+  private LowerTransformer lt;
+
+  @Before
+  public void executeBeforeEach() {
+    BaseTransformer bt = new BaseTransformer();
+    lt = new LowerTransformer(bt);
+  }
+
+  @Test
+  public void testTranformWordToLowerCase() {
+    assertEquals(lt.transform("TeST"), "test");
+  }
+
+  @Test
+  public void testTranformSentenceToLowerCase() {
+    assertEquals(lt.transform("THAT IS FULL SENTENCE."), "that is full sentence.");
+  }
+}
