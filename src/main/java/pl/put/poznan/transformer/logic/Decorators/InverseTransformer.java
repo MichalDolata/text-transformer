2c77c781d83f3b569598d1c00dbffb052ff42833
Łukasz Eckert <luki4824@gmail.com>
2018-12-17 23:23:01 +0100

    Hot fix2

--- src/main/java/pl/put/poznan/transformer/logic/Decorators/InverseTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/Decorators/InverseTransformer.java
@@ -17,19 +17,33 @@ public class InverseTransformer extends Decorator {
     public String transform(String text){
         logger.debug("InverseTransformer input: "+text);
         text = super.transform(text);
-        char lit[] = text.toCharArray();
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList(text.split(" ")));
+        int number_of_words = words.size();
+        int i = 0;
+        while (i  < number_of_words) {
+            String word1 = words.get(i);
+            words.set(i, this.inverse(word1));
+            i++;
 
+        }
+
+        text = String.join(" ", words);
+        logger.debug("InverseTransformer output: "+text);
+        return text;
+
+    }
+    private String inverse(String text){
+        char lit[] = text.toCharArray();
         char tmp;
-		    int i = 0;
-		    int j = text.size();
-		    while(i < j) {
-			    tmp = lit[i];
-			    lit[i] = lit[j];
-			    lit[j] = tmp;
-			    i++;
-			    j--;		
-		    }
-        logger.debug("InverseTransformer output: "+String.valueOf(lit));
+        int i = 0;
+        int j = text.length();
+        while(i < j) {
+            tmp = lit[i];
+            lit[i] = lit[j];
+            lit[j] = tmp;
+            i++;
+            j--;
+        }
         return String.valueOf(lit);
     }
 
