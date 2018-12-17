cc2157a086249193269f55242c46eed5484a9edd
Lukasz Eckert <luki4824@gmail.com>
2018-12-17 23:53:27 +0100

    ADD: Transformer pipeline

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -2,6 +2,13 @@ package pl.put.poznan.transformer.logic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.put.poznan.transformer.logic.Decorators.CapitalizeTransformer;
+import pl.put.poznan.transformer.logic.Decorators.Decorator;
+import pl.put.poznan.transformer.logic.Decorators.Map.AbbrevivationToTextTransformer;
+import pl.put.poznan.transformer.logic.Decorators.Map.LatexTransformer;
+import pl.put.poznan.transformer.logic.Decorators.Map.TextToAbbrevivationTransformer;
+import pl.put.poznan.transformer.logic.Decorators.RepetitionTransformer;
+import pl.put.poznan.transformer.logic.Decorators.UpperTransformer;
 //import javax.validation.constraints.Null;
 //import java.util.*;
 
@@ -9,4 +16,29 @@ import org.slf4j.LoggerFactory;
 public interface TextTransformer {
     Logger logger = LoggerFactory.getLogger(TextTransformer.class);
     String transform(String text);
+
+    static Decorator getTransformerByName(String name, TextTransformer tt){
+        Decorator res = null;
+        switch (name){
+            case "latex":
+                res = new LatexTransformer(tt);
+                break;
+            case "upper":
+                res = new UpperTransformer(tt);
+                break;
+            case "capitalize":
+                res = new CapitalizeTransformer(tt);
+                break;
+            case "repetition":
+                res = new RepetitionTransformer(tt);
+                break;
+            case "abbrevivationtotext":
+                res = new AbbrevivationToTextTransformer(tt);
+                break;
+            case "texttoabbrevivation":
+                res = new TextToAbbrevivationTransformer(tt);
+                break;
+        }
+        return res;
+    }
 }
--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,10 +1,13 @@
 package pl.put.poznan.transformer.rest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.BaseTransformer;
 import pl.put.poznan.transformer.logic.TextTransformer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
 import java.lang.String;
 
 import java.util.Arrays;
@@ -18,38 +21,47 @@ public class TextTransformerController {
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<String> get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,latex") String[] transforms) {
+                                      @RequestParam(value = "transforms", defaultValue = "upper,latex") String[] transforms) {
 
         // log the parameters
         logger.debug("Input: " + text);
         logger.debug("Transformations: " + Arrays.toString(transforms));
 
         // do the transformation, you should run your logic here, below just a silly example
-     /*   TextTransformer transformer = new TextTransformer(transforms);
+        TextTransformer transformer = new BaseTransformer();
+        for (String trans : transforms) {
+            transformer = TextTransformer.getTransformerByName(trans, transformer);
+            if (transformer == null)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         try {
             return ResponseEntity.ok(transformer.transform(text));
         } catch (IllegalArgumentException exception) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }*/
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<String> post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+                                       @RequestBody String[] transforms) {
 
         // log the parameters
         logger.debug("Input: " + text);
         logger.debug("Transformations: " + Arrays.toString(transforms));
 
-        // do the transformation, you should run your logic here, below just a silly example
-        /*TextTransformer transformer = new TextTransformer(transforms);
+        TextTransformer transformer = new BaseTransformer();
+        for (String trans : transforms) {
+            transformer = TextTransformer.getTransformerByName(trans, transformer);
+            if (transformer == null)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         try {
             return ResponseEntity.ok(transformer.transform(text));
         } catch (IllegalArgumentException exception) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }*/
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
