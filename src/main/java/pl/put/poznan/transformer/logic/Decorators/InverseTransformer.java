6ebd5eeea42bd849d8971b71b5f97ff9747d413f
ievgeniibogdanov <44505206+ievgeniibogdanov@users.noreply.github.com>
2019-01-15 00:23:04 +0100

    Update InverseTransformer.java
    Inverse last

--- src/main/java/pl/put/poznan/transformer/logic/Decorators/InverseTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/Decorators/InverseTransformer.java
@@ -12,37 +12,17 @@ public class InverseTransformer extends Decorator {
     /**
      * This function is used for inversing text on chosen position.
      * @param text - Input string that will be changed
+     * @param text Input String
+     * @return reversed string
      */
     @Override
     public String transform(String text){
         logger.debug("InverseTransformer input: "+text);
         text = super.transform(text);
-        ArrayList<String> words = new ArrayList<String>(Arrays.asList(text.split(" ")));
-        int number_of_words = words.size();
-        int i = 0;
-        while (i  < number_of_words) {
-            String word1 = words.get(i);
-            words.set(i, this.inverse(word1));
-            i++;
-
-        }
-
-        text = String.join(" ", words);
-        logger.debug("InverseTransformer output: "+text);
-        return text;
-
-    }
-
-    /**
-     *  This function inverse word.
-     * @param text Input String
-     * @return reversed string
-     */
-    private String inverse(String text){
         char lit[] = text.toCharArray();
         char tmp;
         int i = 0;
-        int j = text.length();
+        int j = text.length()-1;
         while(i < j) {
             tmp = lit[i];
             lit[i] = lit[j];
@@ -50,6 +30,7 @@ public class InverseTransformer extends Decorator {
             i++;
             j--;
         }
+	logger.debug("InverseTransformer output: "+String.valueOf(lit));
         return String.valueOf(lit);
     }
 
